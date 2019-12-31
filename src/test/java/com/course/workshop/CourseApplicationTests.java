11b1a93bbfa73389f5902a25c74844eb7a28f8ff
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2019-12-31 18:34:18 -0300

    first commit

---
+++ src/test/java/com/course/workshop/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
